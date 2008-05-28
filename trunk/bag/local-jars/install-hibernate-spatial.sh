519f749704c1209b6446e0e4f7ccb40868538eba
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-05-28 20:24:46 +0000

    linux support

---
+++ trunk/bag/local-jars/install-hibernate-spatial.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-spatial -Dversion=1.0M1 -Dpackaging=jar -Dfile=hibernate-spatial-1.0-M1.jar
---
+++ trunk/bag/local-jars/install-jts.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mvn install:install-file -DgroupId=com.vividsolutions -DartifactId=jts -Dversion=1.8 -Dpackaging=jar -Dfile=jts-1.8.jar
