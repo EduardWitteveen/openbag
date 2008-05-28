59a0fd6d38e71456c199078dfe346bcd12a9da0a
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-05-28 14:05:03 +0000

    do not generate the id automatic!

--- bag/src/main/java/org/nergens/bag/storage/pojo/BagObject.java
+++ bag/src/main/java/org/nergens/bag/storage/pojo/BagObject.java
@@ -18,7 +18,6 @@ import javax.persistence.*;
 @Table(name="DATA_OBJECT")
 public abstract class BagObject {    
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name="CODE")
     protected long code;
     public long getCode() {
