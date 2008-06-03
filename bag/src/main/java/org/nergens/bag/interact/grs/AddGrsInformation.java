3927314cd7ef45f58018ef6a93d68f40ccedb517
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-06-03 15:53:52 +0000

    - additional contains in sql - smarter comparison of straat/huisnummer/woonplaats, with double check method, to see if sql result is valid

--- bag/src/main/java/org/nergens/bag/interact/grs/AddGrsInformation.java
+++ bag/src/main/java/org/nergens/bag/interact/grs/AddGrsInformation.java
@@ -9,7 +9,9 @@ import java.sql.Connection;
 import java.sql.SQLSyntaxErrorException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 import oracle.jdbc.pool.OracleDataSource;
@@ -33,23 +35,33 @@ public class AddGrsInformation {
     private static final java.lang.String BAG_ORACLE_SID = "";
     private static final java.lang.String BAG_ORACLE_USERNAME = "";
     private static final java.lang.String BAG_ORACLE_PASSWORD = "";
-    private static final java.lang.String BAG_ORACLE_SQL = "SELECT *\n" + 
-    "     FROM (\n" +
-    "       SELECT\n" +
-    "         SUBSTR('00000000000000000000', 20 - LENGTH(STUKNR))\n" +
-    "         || \n" +
-    "         STUKNR AS DOCUMENTNR,\n" + 
-    "         TO_DATE(STUKDAT) DOCUMENTDATUM,\n" +  
-    "         OMSLANG DOCUMENTOMSCHRIJVING\n" +
-    "       FROM GRS.STUK\n" +
-    "       UNION SELECT \n" +
-    "         SUBSTR('dossier:000000000000', 20 - LENGTH(DOSSIERKODE))\n" +
-    "         ||   \n" +
-    "         DOSSIERKODE,\n" + 
-    "         NULL, \n" +
-    "         OMSLANG\n" +
-    "       FROM GRS.DOSSIER\n" +
-    "     ) DOCUMENT\n";
+    private static final java.lang.String BAG_ORACLE_SQL = "SELECT \n" +
+    	"           DOCUMENTNR, \n" +
+    	"           DOCUMENTDATUM, \n" +
+    	"           DOCUMENTOMSCHRIJVING\n" +
+    	"              FROM (\n" +
+    	"                SELECT\n" +
+    	"                  SUBSTR('00000000000000000000', 0, (20 - LENGTH(STUKNR)))\n" +
+    	"                  || \n" +
+    	"                  STUKNR AS DOCUMENTNR,\n" +
+    	"                  TO_DATE(STUKDAT) DOCUMENTDATUM,\n" +
+    	"                  OMSLANG DOCUMENTOMSCHRIJVING,\n" +
+    	"                  CLS CLASSIFICATIE\n" +
+    	"                FROM GRS.STUK\n" +
+    	"                UNION SELECT \n" +
+    	"                  SUBSTR('dossier:000000000000', 0, (20 - LENGTH(DOSSIERKODE)))\n" +
+    	"                  ||   \n" +
+    	"                  DOSSIERKODE,\n" +
+    	"                 CASE \n" +
+    	"                   WHEN ARCHIEFNAAM = 'KELDERB' THEN TO_DATE('01-JAN-1998')\n" +
+    	"                   ELSE TO_DATE('01-JAN-1998')\n" +
+    	"                  END, \n" +
+    	"                  OMSLANG,\n" +
+    	"                  CLASSIFICATIE\n" +         
+    	"                FROM GRS.DOSSIER\n" +
+    	"              ) DOCUMENT\n" +
+    	"    WHERE CLASSIFICATIE = 'B'\n";
+    
     private static Connection getConnection() throws java.sql.SQLException {        
         log.info("java.library.path:" + System.getProperty("java.library.path"));
         // open bag connection
@@ -119,38 +131,129 @@ public class AddGrsInformation {
 	            	filter += huisnummertoevoeging.toUpperCase() + "%";
 	            }
 	            filter += woonplaatsnaam.toUpperCase();
-	            sql += "    WHERE UPPER(DOCUMENTOMSCHRIJVING) LIKE ('" + filter +"')\n";
+	            sql += "    AND UPPER(DOCUMENTOMSCHRIJVING) LIKE ('" + filter +"')\n";
 	            // find the minimal string with this result, and the latest result
 	            sql += "    ORDER BY LENGTH(DOCUMENTOMSCHRIJVING) ASC, DOCUMENTDATUM DESC";
-	            log.info(sql);
+	            //log.info(sql);
 	            java.sql.Statement statement = connection.createStatement();
 	            java.sql.ResultSet recordset = statement.executeQuery(sql);
-	            if(recordset.next()) {	                
+	            while(recordset.next()) {	                
 	                String documentnr = recordset.getString("DOCUMENTNR");
 	                String documentdatum = recordset.getString("DOCUMENTDATUM");
 	                String documentomschrijving = recordset.getString("DOCUMENTOMSCHRIJVING");
-	                // TODO: see if documentomschrijving - matching fields does not contain any characters!
-	                verblijfsobject.setDocumentNummer(documentnr);
-	                Format formatter = new SimpleDateFormat("DD-MMM-yy");
-	                if(documentdatum != null) {
-	                	verblijfsobject.setDocumentDatum((java.util.Date)formatter.parseObject(documentdatum));
+	                
+	                // DOUBLE CHECK!
+	                // check if documentomschrijving is the same thing as we are searhcing for
+	                String[] details = getVergunningDetails(documentomschrijving);
+	                if(details[0].equals(straat.getNaam().toUpperCase())) {
+	                	String nummeraanduiding = nummer.getHuisnummer().toString();
+	                	if(nummer.getHuisletter() != null ) nummeraanduiding += nummer.getHuisletter();
+	                	if(nummer.getHuisnummertoevoeging() != null ) nummeraanduiding += nummer.getHuisnummertoevoeging();
+	                	if (details[1].equals(nummeraanduiding.toUpperCase())) {
+	                		if (details[2].equals(woonplaats.getNaam().toUpperCase())) {
+	    		                verblijfsobject.setDocumentNummer(documentnr);
+	    		                Format formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SS");
+	    		                if(documentdatum != null) {
+	    		                	verblijfsobject.setDocumentDatum((java.util.Date)formatter.parseObject(documentdatum));
+	    		                }
+	    		                log.info("SAVE:" + verblijfsobject + " OMSCHRIJVING:" + documentomschrijving + " DOCUMENTNR:" + documentnr);
+	    		                session.save(verblijfsobject);
+	    		                break;
+	                		}
+	                		else log.info("RECORD DOES NOT MATCH ON WOONPLAATS(" + woonplaats + " !=  " + details[2] + "):" + documentomschrijving);
+	                	}
+		                //else log.info("RECORD DOES NOT MATCH ON NUMBER(" + nummeraanduiding + " !=  " + details[1] + "):" + documentomschrijving);	                	
 	                }
-	                session.save(verblijfsobject);
+	                else log.info("RECORD DOES NOT MATCH ON STRAAT(" + straat  + " !=  " + details[0] + "):" + documentomschrijving);
 	            }	            	            
 	            recordset.close();
 	            statement.close();	        
         	}
         	catch(SQLSyntaxErrorException ssee) {
-        			log.info(sql);
-        			ssee.printStackTrace();
+        		log.info(sql);
+        		ssee.printStackTrace();
         	}
         	catch(Exception ex) {
+        		log.info(sql);
         		ex.printStackTrace();
         	}
         }
-        session.getTransaction().commit();
-        
+        session.getTransaction().commit();        
         connection.close();
         // datasource.close();
+    }
+    private static String[] getVergunningDetails(String str) {    	    	
+    	/**
+    	 * Bouwvergunningen, Amen 26c, Amen
+    	 * Bouwvergunningen, 't Veld 17, Annen
+    	 */    	
+    	String todo = str.toUpperCase();
+    	String search = "BOUWVERGUNNINGEN";
+    	int pos = todo.indexOf(search);
+    	if(pos == -1) {
+    		search = "BOUWVERGUNNING";
+    		pos = todo.indexOf(search);
+    	}    	    	
+    	if(pos == -1) {
+    		log.info("BOUWVERGUNNING NOT FOUND:" + str);
+    		return null;    		
+    	}
+    	todo = todo.substring(pos + search.length());
+    	while(todo.length() > 0 
+    	    	// skip any leading "," or " "-es
+    			&&(todo.charAt(0) == ' ' || todo.charAt(0) == ','))
+    	{
+    		todo = todo.substring(1);
+    	}    	
+    	// we can fill the straatnaam
+    	String straatnaam = "";
+    	while(todo.length() > 0 
+    			// we like everything, except a number (housenumbers typically start with that kinda info)
+    			&&(todo.charAt(0) < '0' || todo.charAt(0) > '9'))
+    	{    		
+    		char c = todo.charAt(0);
+    		straatnaam += c;
+    		todo = todo.substring(1);
+    	}
+    	// now trim the straatnaam
+    	straatnaam = straatnaam.trim();
+    	
+    	// the part at the end should be the city, and it ends with the ","
+    	String woonplaatsnaam = "";
+    	while(todo.length() > 0 
+    			// seach till the beginnign of the woonplaats
+    			&&((todo.charAt(todo.length() -1) != ',') && todo.charAt(todo.length() -1) != ' '))
+    	{    		
+    		char c = todo.charAt(todo.length() -1);
+    		woonplaatsnaam = c + woonplaatsnaam;  
+    		todo = todo.substring(0, todo.length()  - 1);
+    	}
+    	woonplaatsnaam = woonplaatsnaam.trim();    	
+    	while(todo.length() > 0 
+    	    	// skip any leading "0"
+    			&&(todo.charAt(0) == '0'))
+    	{
+    		todo = todo.substring(1);
+    	}
+    	while(todo.length() > 0 
+    	    	// skip any trailing "," or " "-es
+    			&&(todo.charAt(todo.length() -1) == ' ' || todo.charAt(todo.length() -1) == ','))
+    	{
+    		todo = todo.substring(0, todo.length()  - 1);
+    	}
+    	String nummeraanduiding = todo;
+    	if(straatnaam.length() == 0) {
+    		log.info("no straatnaam for:" + str);
+    	}
+    	else if(woonplaatsnaam.length() == 0) {
+    		log.info("no woonplaats for:" + str);
+    	}
+    	else if(nummeraanduiding.length() == 0) {
+    		log.info("no nummeraanduiding for:" + str);
+    	}
+    	else {
+    		log.info("omschrijving:"+ str +"\n\tstraat:"+ straatnaam+"\n\tnummer:"+nummeraanduiding+"\n\twoonplaats:"+woonplaatsnaam);
+    	}
+    	return new String[]{straatnaam, nummeraanduiding, woonplaatsnaam};
     }
 }
