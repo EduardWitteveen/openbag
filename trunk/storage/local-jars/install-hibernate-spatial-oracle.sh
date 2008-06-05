259ceb69ba03156b5c3a538633c4e937a7faf69b
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-06-05 19:33:35 +0000

    oracle 9i fix, could not insert null values into the database

---
+++ trunk/storage/local-jars/install-hibernate-spatial-oracle.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-spatial-oracle -Dversion=1.0M1 -Dpackaging=jar -Dfile=hibernate-spatial-oracle-1.0-M1.jar
--- trunk/storage/src/main/java/org/nergens/bag/storage/util/OracleSpatial9iDialect.java
+++ trunk/storage/src/main/java/org/nergens/bag/storage/util/OracleSpatial9iDialect.java
@@ -3,18 +3,33 @@ package org.nergens.bag.storage.util;
 import java.sql.Types;
 import org.hibernatespatial.SpatialDialect;
 import org.hibernatespatial.oracle.OracleSpatial10gDialect;
+import org.hibernatespatial.oracle.SDOGeometryType;
 
 public class OracleSpatial9iDialect extends OracleSpatial10gDialect implements SpatialDialect {
-//public class OracleSpatial9iDialect extends org.hibernatespatial.oracle.OracleSpatial10gDialect {
+    private static String SQL_TYPE_NAME = "MDSYS.SDO_GEOMETRY";
+    
     public OracleSpatial9iDialect() {
         super();
-        // JDBC type: 2002 = Types.STRUCT
-        //  error:  SEVERE: could not complete schema update
-        //          org.hibernate.MappingException: No Dialect mapping for JDBC type: 2002
-        //  or:     type geometry is used, while MDSYS.SDO_GEOMETRY should be used!
-        // 
-        
         // override the registerColumnType of super class
-        registerColumnType(Types.STRUCT, "MDSYS.SDO_GEOMETRY");        
+        registerColumnType(Types.STRUCT, SQL_TYPE_NAME);        
+        //  HACK: on code from subversion:
+        //       http://www.hibernatespatial.org/svn/hibernate-spatial/tags/1.0-M1/
+        //  follow patch is required:
+        /*
+            Index: SDOGeometryType.java
+            ===================================================================
+            --- SDOGeometryType.java	(revision 96)
+            +++ SDOGeometryType.java	(working copy)
+            @@ -75,7 +75,7 @@
+
+                    private static final int[] geometryTypes = new int[] { Types.STRUCT };
+
+            -	private static String SQL_TYPE_NAME = "SDO_GEOMETRY";
+            +	public static String SQL_TYPE_NAME = "SDO_GEOMETRY";
+
+                    @Override
+                    public void nullSafeSet(PreparedStatement st, Object value, int index)
+        */
+        SDOGeometryType.SQL_TYPE_NAME = SQL_TYPE_NAME;
     }
 }
