cf7256c0564289a860b16dde7e35b13eba460f34
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-06-11 08:29:02 +0000

    Still the oracle 9i problems

--- trunk/storage/src/main/java/org/hibernatespatial/oracle/OracleSpatial9iDialect.java
+++ trunk/storage/src/main/java/org/hibernatespatial/oracle/OracleSpatial9iDialect.java
@@ -8,26 +8,7 @@ public class OracleSpatial9iDialect extends OracleSpatial10gDialect implements S
     
     public OracleSpatial9iDialect() {
         super();
-        // override the registerColumnType of super class
-        registerColumnType(Types.STRUCT, SQL_TYPE_NAME);        
-        //  HACK: on code from subversion:
-        //       http://www.hibernatespatial.org/svn/hibernate-spatial/tags/1.0-M1/
-        //  follow patch is required:
-        /*
-            Index: SDOGeometryType.java
-            ===================================================================
-            --- SDOGeometryType.java	(revision 96)
-            +++ SDOGeometryType.java	(working copy)
-            @@ -75,7 +75,7 @@
-
-                    private static final int[] geometryTypes = new int[] { Types.STRUCT };
-
-            -	private static String SQL_TYPE_NAME = "SDO_GEOMETRY";
-            +	static String SQL_TYPE_NAME = "SDO_GEOMETRY";
-
-                    @Override
-                    public void nullSafeSet(PreparedStatement st, Object value, int index)
-        */
+        registerColumnType(Types.STRUCT, SQL_TYPE_NAME);
         SDOGeometryType.SQL_TYPE_NAME = SQL_TYPE_NAME;
     }
 }
