42929193350d5011e8e46c0ae2350e0fb3cbfaff
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-07-02 13:45:24 +0000

    now we can use other servers

--- trunk/php/config.php
+++ trunk/php/config.php
@@ -1,23 +1,24 @@
-# CONFIG FILE FOR PHP-BAG IN INI-FORMAT<?php /*
+# CONFIG FILE FOR PHP-BAG IN INI-FORMAT<?php /**/
+#used by soap communication
 [internet]
-#used by frontoffice and midoffice
-proxy_host		=	"proxyserver"
-proxy_port		=	8888
-proxy_login		=	"username"
-proxy_password	=	"password"
+#proxy_host		=	"proxyserver"
+#proxy_port		=	8888
+#proxy_login		=	"username"
+#proxy_password	=	"password"
 
-[frontoffice]
 #used by front office
+[frontoffice]
 midoffice		=	"../midoffice/config.php?wsdl"
+#midoffice		=	"http://192.168.198.118:8080/php/gui/midoffice/config.php?wsdl"	
 
-[midoffice]
 #used by midoffice
-bag-wsdl		=	"../midoffice/bag/bag.php?wsdl"
+[midoffice]
 bag-gis			=	"../../gis/viewer.php"
+#bag-gis			=	"http://192.168.198.118:8080/php/gui/gis/viewer.php"
 
-[database]
 #used by midoffice and editor
-tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oracleserver)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
-username		=	"username"
-password		= 	"password"
+[database]
+tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oraserver)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
+username		=	"beheerder"
+password		= 	"mart!n"
 #  */ ?>
