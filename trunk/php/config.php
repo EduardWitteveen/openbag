f64d8727ecb2b752084ead6fcf962c911904aed2
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-07-03 09:22:43 +0000

    midoffice and gisserver can be configured

--- trunk/php/config.php
+++ trunk/php/config.php
@@ -1,24 +1,24 @@
 # CONFIG FILE FOR PHP-BAG IN INI-FORMAT<?php /**/
-#used by soap communication
+#used by soap communication (debug with fiddler)
 [internet]
-#proxy_host		=	"proxyserver"
+#proxy_host		=	"localhost"
 #proxy_port		=	8888
 #proxy_login		=	"username"
-#proxy_password	=	"password"
+#proxy_password		=	"password"
 
 #used by front office
 [frontoffice]
 midoffice		=	"../midoffice/config.php?wsdl"
-#midoffice		=	"http://192.168.198.118:8080/php/gui/midoffice/config.php?wsdl"	
+#midoffice		=	"http://midofficeserver/php/gui/midoffice/config.php?wsdl"	
 
 #used by midoffice
 [midoffice]
 bag-gis			=	"../../gis/viewer.php"
-#bag-gis			=	"http://192.168.198.118:8080/php/gui/gis/viewer.php"
+#bag-gis			=	"http://gisserver/php/gui/gis/viewer.php"
 
 #used by midoffice and editor
 [database]
-tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oraserver)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
-username		=	"beheerder"
-password		= 	"mart!n"
+tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = databaseserver)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
+username		=	"username"
+password		= 	"password"
 #  */ ?>
