a68d8bcf6737ae0f341dd3f750a921a75853fb25
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-07-03 10:33:04 +0000

    still config stuff

--- php/config.php
+++ php/config.php
@@ -1,4 +1,3 @@
-# CONFIG FILE FOR PHP-BAG IN INI-FORMAT<?php /**/
 #used by soap communication
 [internet]
 proxy_host		=	"localhost"
@@ -13,14 +12,13 @@ config		=	"http://192.168.198.118:8080/php/gui/midoffice/config.php?wsdl"
 
 #used by midoffice
 [midoffice]
-#bag-service			=	"../gui/midoffice/bag/bag.php?wsdl"	
-bag-service		=	"http://midserver/php/gui/midoffice/bag/bag.php?wsdl"
-#bag-gis			=	"../../gis/viewer.php"
-bag-gis			=	"http://gisserver/php/gui/gis/viewer.php"
+#bagservice			=	"../gui/midoffice/bag/bag.php?wsdl"	
+bagservice		=	"http://midserver/php/gui/midoffice/bag/bag.php?wsdl"
+#baggis			=	"../../gis/viewer.php"
+baggis			=	"http://gisserver/php/gui/gis/viewer.php"
 
 #used by midoffice and editor
 [database]
 tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = servername)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
 username		=	"username"
 password		= 	"password"
-#  */ ?>
