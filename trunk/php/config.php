1ff3849e1d50ceb0b39d4b796ba2ba5bd91ed51a
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-07-03 14:28:22 +0000

    working config

--- trunk/php/config.php
+++ trunk/php/config.php
@@ -1,14 +1,14 @@
 #used by soap communication
 [internet]
-proxy_host		=	"localhost"
+proxy_host		=	"127.0.0.1"
 proxy_port		=	8888
-#proxy_login		=	"username"
-#proxy_password		=	"password"
+proxy_login		=	"username"
+proxy_password	=	"password"
 
 #used by front office
 [frontoffice]
 midoffice		=	"http://192.168.198.118:8080/php/gui/midoffice/config.php?wsdl"	
-#config		=	"../midoffice/config.php?wsdl"
+#midoffice		=	"../midoffice/config.php?wsdl"
 
 #used by midoffice
 [midoffice]
@@ -19,6 +19,6 @@ baggis			=	"http://gisserver/php/gui/gis/viewer.php"
 
 #used by midoffice and editor
 [database]
-tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = servername)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
+tnsname			=	"oci:dbname=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = databaseservername)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = bag)));charset=UTF8"
 username		=	"username"
 password		= 	"password"
