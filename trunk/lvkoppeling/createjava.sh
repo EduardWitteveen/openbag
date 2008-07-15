ab646710a4577bf365e8f947424538b151348128
eywitteveen <eywitteveen@a5440455-474e-0410-a20b-1394c66f8fc1>
2008-07-15 17:40:46 +0000

    added generated java code

--- trunk/lvkoppeling/createjava.sh
+++ trunk/lvkoppeling/createjava.sh
@@ -1,5 +1,6 @@
 wget http://apache.hippo.nl/ws/axis2/1_4/axis2-1.4-bin.zip
 unzip axis2-1.4-bin.zip
 chmod +x axis2-1.4/bin/wsdl2java.sh
-axis2-1.4/bin/wsdl2java.sh -uri 06-StUF0206/bag/0120/bagstuf.wsdl
-axis2-1.4/bin/wsdl2java.sh -uri 06-StUF0206/bag/0120/bag-sync-stuf.wsdl
+# into src/main/java
+axis2-1.4/bin/wsdl2java.sh -S src/main/java -uri 06-StUF0206/bag/0120/bagstuf.wsdl
+axis2-1.4/bin/wsdl2java.sh -S src/main/java -uri 06-StUF0206/bag/0120/bag-sync-stuf.wsdl
